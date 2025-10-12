5ea4579612ae1aadf99e3d5fda78b7b3bc5e37f8
arekflis <arkadiuszflisikowski10@gmail.com>
2025-10-13 01:44:17 +0200

    Modify EntityBase class

--- university-course-manager/src/main/java/pl/edu/pg/eti/kask/ucm/entity/baseEntity/BaseEntity.java
+++ university-course-manager/src/main/java/pl/edu/pg/eti/kask/ucm/entity/baseEntity/BaseEntity.java
@@ -5,12 +5,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
 @Getter
 @Setter
+@SuperBuilder
 @MappedSuperclass
 public abstract class BaseEntity {
 
