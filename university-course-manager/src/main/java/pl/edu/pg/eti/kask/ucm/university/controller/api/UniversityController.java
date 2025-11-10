b9a7201e7682af50aa896d3083a70a40153a80a8
arekflis <arkadiuszflisikowski10@gmail.com>
2025-11-10 23:47:11 +0100

    Refactor UniversityController

--- university-course-manager/src/main/java/pl/edu/pg/eti/kask/ucm/university/controller/api/UniversityController.java
+++ university-course-manager/src/main/java/pl/edu/pg/eti/kask/ucm/university/controller/api/UniversityController.java
@@ -1,5 +1,7 @@
 package pl.edu.pg.eti.kask.ucm.university.controller.api;
 
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
 import pl.edu.pg.eti.kask.ucm.university.dto.request.PatchUniversityRequest;
 import pl.edu.pg.eti.kask.ucm.university.dto.request.PutUniversityRequest;
 import pl.edu.pg.eti.kask.ucm.university.dto.response.GetUniversitiesResponse;
@@ -7,14 +9,35 @@ import pl.edu.pg.eti.kask.ucm.university.dto.response.GetUniversityResponse;
 
 import java.util.UUID;
 
+@Path("")
 public interface UniversityController {
 
+    @GET
+    @Path("/universities")
+    @Produces(MediaType.APPLICATION_JSON)
     GetUniversitiesResponse getUniversities();
-    GetUniversityResponse getUniversityById(UUID id);
-    GetUniversitiesResponse getUniversitiesByCity(String city);
 
-    void putUniversity(UUID id, PutUniversityRequest request);
-    void patchUniversity(UUID id, PatchUniversityRequest request);
+    @GET
+    @Path("/universities/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    GetUniversityResponse getUniversityById(@PathParam("id") UUID id);
 
-    void deleteUniversity(UUID id);
+    @GET
+    @Path("/universities/{city}")
+    @Produces(MediaType.APPLICATION_JSON)
+    GetUniversitiesResponse getUniversitiesByCity(@PathParam("city") String city);
+
+    @PUT
+    @Path("/universities/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    void putUniversity(@PathParam("id") UUID id, PutUniversityRequest request);
+
+    @PATCH
+    @Path("/universities/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    void patchUniversity(@PathParam("id") UUID id, PatchUniversityRequest request);
+
+    @DELETE
+    @Path("/universities/{id}")
+    void deleteUniversity(@PathParam("id") UUID id);
 }
