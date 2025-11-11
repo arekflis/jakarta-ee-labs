7713fb11b8895c60b6b7977b46a781a15c17c71a
arekflis <arkadiuszflisikowski10@gmail.com>
2025-11-12 00:11:13 +0100

    Fix compilation errors

--- university-course-manager/src/main/java/pl/edu/pg/eti/kask/ucm/tutor/repository/impl/TutorInMemoryRepository.java
+++ university-course-manager/src/main/java/pl/edu/pg/eti/kask/ucm/tutor/repository/impl/TutorInMemoryRepository.java
@@ -1,8 +1,6 @@
 package pl.edu.pg.eti.kask.ucm.tutor.repository.impl;
 
 import jakarta.enterprise.context.RequestScoped;
-import jakarta.inject.Inject;
-import pl.edu.pg.eti.kask.ucm.datastore.component.DataStore;
 import pl.edu.pg.eti.kask.ucm.tutor.entity.Tutor;
 import pl.edu.pg.eti.kask.ucm.tutor.repository.api.TutorRepository;
 
@@ -13,46 +11,28 @@ import java.util.UUID;
 @RequestScoped
 public class TutorInMemoryRepository implements TutorRepository {
 
-    private final DataStore dataStore;
-
-    @Inject
-    public TutorInMemoryRepository(DataStore dataStore) {
-        this.dataStore = dataStore;
-    }
-
     @Override
     public Optional<Tutor> find(UUID id){
-        return this.dataStore.findAllTutors().stream()
-                .filter(tutor -> tutor.getId().equals(id))
-                .findFirst();
+        return null;
     }
 
     @Override
     public List<Tutor> findAll(){
-        return this.dataStore.findAllTutors();
+        return null;
     }
 
     @Override
-    public void create(Tutor entity){
-        this.dataStore.createTutor(entity);
-    }
+    public void create(Tutor entity){ }
 
     @Override
-    public void delete(UUID id){
-        this.dataStore.deleteTutor(id);
-    }
+    public void delete(UUID id) { }
 
     @Override
-    public void update(Tutor entity){
-        this.dataStore.updateTutor(entity);
-    }
+    public void update(Tutor entity) { }
 
     @Override
     public Optional<Tutor> findByEmail(String email){
-        return this.dataStore.findAllTutors().stream()
-                .filter(tutor -> tutor.getEmail().equals(email))
-                .findFirst();
+        return null;
     }
-
 
 }
