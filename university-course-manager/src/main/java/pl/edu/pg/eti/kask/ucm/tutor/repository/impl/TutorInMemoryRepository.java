5d51d7da2ca5ba942b534464034e10fe56fa7865
arekflis <arkadiuszflisikowski10@gmail.com>
2025-11-17 21:44:39 +0100

    Update TutorRepository

--- university-course-manager/src/main/java/pl/edu/pg/eti/kask/ucm/tutor/repository/impl/TutorInMemoryRepository.java
+++ university-course-manager/src/main/java/pl/edu/pg/eti/kask/ucm/tutor/repository/impl/TutorInMemoryRepository.java
@@ -1,6 +1,8 @@
 package pl.edu.pg.eti.kask.ucm.tutor.repository.impl;
 
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import pl.edu.pg.eti.kask.ucm.tutor.entity.Tutor;
 import pl.edu.pg.eti.kask.ucm.tutor.repository.api.TutorRepository;
 
@@ -11,28 +13,42 @@ import java.util.UUID;
 @RequestScoped
 public class TutorInMemoryRepository implements TutorRepository {
 
+    private EntityManager em;
+
+    @PersistenceContext
+    public void setEm(EntityManager em) {
+        this.em = em;
+    }
+
     @Override
     public Optional<Tutor> find(UUID id){
-        return null;
+        return Optional.ofNullable(this.em.find(Tutor.class, id));
     }
 
     @Override
     public List<Tutor> findAll(){
-        return null;
+        return this.em.createQuery("select t from Tutor t", Tutor.class).getResultList();
     }
 
     @Override
-    public void create(Tutor entity){ }
+    public void create(Tutor entity){
+        this.em.persist(entity);
+    }
 
     @Override
-    public void delete(UUID id) { }
+    public void delete(UUID id) {
+        this.em.remove(this.em.find(Tutor.class, id));
+    }
 
     @Override
-    public void update(Tutor entity) { }
+    public void update(Tutor entity) {
+        this.em.merge(entity);
+    }
 
     @Override
     public Optional<Tutor> findByEmail(String email){
-        return null;
+        return Optional.ofNullable(this.em.createQuery("select t from Tutor t where t.email = :email", Tutor.class)
+                .setParameter("email", email)
+                .getSingleResult());
     }
-
 }
